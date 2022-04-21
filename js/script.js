f71e26d0fbd7b2e8c6d74fc7b1868c2fc5dcb21b
Atanu Shuvam Roy <atanuroy911@gmail.com>
2022-04-21 19:52:07 +0600

    modal added

--- js/script.js
+++ js/script.js
@@ -31,11 +31,12 @@ const getSingleResult = async (slug) => {
 
 const displayResult = data => {
     const searchResults = document.getElementById('search-results');
-    const detailsModal = document.getElementById('detials-section');
+    const detailsModal = document.getElementById('details-section');
     searchResults.innerHTML = '';
     data.data.forEach(async (element) => {
         const singleData = await getSingleResult(element.slug);
         const div = document.createElement('div');
+        const modalDiv = document.createElement('div');
         div.classList.add('col');
         let releaseDate;
         if (!singleData.data.releaseDate || singleData.data.releaseDate == '') {
@@ -44,6 +45,7 @@ const displayResult = data => {
         else {
             releaseDate = singleData.data.releaseDate;
         }
+
         const searchResult = `
                     <div class="card h-100">
                         <img src="${element.image}"
@@ -59,12 +61,102 @@ const displayResult = data => {
                             </p>
                         </div>
                         <div class="m-3 text-end">
-                            <button id="details-button" class="btn btn-primary">Explore</button>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#${element.slug}">
+                            Explore
+                        </button>
                         </div>
+                        
                     </div>
         `;
+        const modals = `
+                <div class="modal fade" id="${element.slug}" tabindex="-1" aria-labelledby="${element.slug}Label"
+                aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="${element.slug}Label">${element.brand} ${element.phone_name}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                                <div class="row">
+                                    <div class="col-5 justify-content-center align-items-center">
+                                        <img src="${element.image}"
+                                            class="card-img-top w-75 mx-auto py-3 img-fluid" alt="...">
+                                    </div>
+                                    <div class="col-7">
+                                        <h5>Specifications</h5>
+                                        <table class="table">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">Features</th>
+                                                    <th scope="col">Details</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <th scope="row">Chipset</th>
+                                                    <td>${singleData.data.mainFeatures.chipSet}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Display</th>
+                                                    <td>${singleData.data.mainFeatures.displaySize}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Memory</th>
+                                                    <td>${singleData.data.mainFeatures.memory}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Storage</th>
+                                                    <td>${singleData.data.mainFeatures.storage}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Sensors</th>
+                                                    <td>${singleData.data.mainFeatures.sensors}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Wi-Fi</th>
+                                                    <td>${singleData.data.others.WLAN}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Bluetooth</th>
+                                                    <td>${singleData.data.others.Bluetooth}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">GPS</th>
+                                                    <td>${singleData.data.others.GPS}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">NFC</th>
+                                                    <td>${singleData.data.others.NFC}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Radio</th>
+                                                    <td>${singleData.data.others.Radio}</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">USB</th>
+                                                    <td>${singleData.data.others.USB}</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+
+                            </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
         div.innerHTML = searchResult;
+        modalDiv.innerHTML = modals;
         searchResults.appendChild(div);
+        detailsModal.appendChild(modalDiv);
     });
     toggleSpinner('none');
 }
